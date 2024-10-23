3856626a9fe029c9ebbe99dcdee8ebae30573aac
Anton Maksimov <anton.maximov@gmail.com>
2024-10-23 19:12:49 +0300

    create objects animal, dog and cat

--- main.go
+++ main.go
@@ -2,4 +2,16 @@ package main
 
 func main() {
 	println("Hello World")
+
+	animal := Animal{}
+	animal.Say()
+
+	dog := Dog{}
+	dog.Color = "red"
+	dog.Weight = 10
+	dog.Say()
+
+	cat := Cat{}
+	cat.Say()
+
 }
